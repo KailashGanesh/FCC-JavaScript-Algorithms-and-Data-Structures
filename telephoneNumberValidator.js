f5f6d3d17b345b8f07c8248dfee3a5ac381bf18d
Kailash <58307685+KailashGanesh@users.noreply.github.com>
2021-06-26 19:12:00 +0530

    finish phone validator

--- telephoneNumberValidator.js
+++ telephoneNumberValidator.js
@@ -1,8 +1,27 @@
 function telephoneCheck(str) {
+    if (str[0] === "-"){
+        return false
+    }
+    
+    let tester = 0
+    for (let i = 0; i < str.length; i++){
+        if (str[i] === "-"){
+            tester += 1
+        }
+    }
+
+    if (tester > 2){
+        return false
+    }
+    console.log("index of -",str.indexOf("-"))
+    // if (str.indexOf("-") < 3 ){
+    //     console.log("hello")
+    //     return false;
+    // }
+
     let strippedPhoneNumber = str.replace(/-| |/g,'')
 
     console.log(strippedPhoneNumber)
-// \(|\)
 
     if (strippedPhoneNumber.indexOf("(") < strippedPhoneNumber.indexOf(")") && strippedPhoneNumber.indexOf("(") !== -1 &&  strippedPhoneNumber.indexOf(")") < 6){
         console.log("hello")
@@ -31,3 +50,5 @@ console.log(telephoneCheck("(555-555-5555"));
 console.log(telephoneCheck("(6054756961)"));
 console.log(telephoneCheck("-1 (757) 622-7382"));
 console.log(telephoneCheck("55 55-55-555-5"));
+
+console.log("ALL TESTS PASSED")
