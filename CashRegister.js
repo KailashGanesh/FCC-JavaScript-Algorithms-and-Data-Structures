365ad8abecd7f0688c2af787895bef37ca6115cb
Kailash <58307685+KailashGanesh@users.noreply.github.com>
2021-07-08 14:06:48 +0530

    insufficient_funds and 0.5 quater works

--- CashRegister.js
+++ CashRegister.js
@@ -1,28 +1,50 @@
+const USD = [
+    ["PENNY", 1], ["NICKEL", 5], ["DIME", 10], 
+    ["QUARTER", 25], ["ONE", 100], ["FIVE", 500], 
+    ["TEN", 1000], ["TWENTY", 2000], ["ONE HUNDRED", 10000]
+];
+
 function checkCashRegister(price, cash, cid) {
-    let change = cid;
-    let status = "CLOSED"
+    let change = [];
+    let status = "INSUFFICIENT_FUNDS";
+    let moneyToReturn = cash*100 - price*100; // price of product - cash customer gave = return 
+    let isCashRegisterEmpty = false;
 
-    const USD = [
-        ["PENNY", 1], ["NICKEL", 5], ["DIME", 10], 
-        ["QUARTER", 25], ["ONE", 100], ["FIVE", 500], 
-        ["TEN", 1000], ["TWENTY", 2000], ["ONE HUNDRED", 10000]
-    ]
+    let cidIndex = cid.length - 1;
+    while(cidIndex >= 0){
+        if (cid[cidIndex][1] === 0){
+            isCashRegisterEmpty = true
+        }
 
-    // price of product - cash customer gave = return 
-    let moneyReturn = cash - price
-    console.log(moneyReturn);
-
-    if (moneyReturn){
-        status = "OPEN"
+        cidIndex -= 1
     }
 
+    if (moneyToReturn && !isCashRegisterEmpty){
+        let i = USD.length - 1;
+        while ( i >= 0) {
+            let moneyName = USD[i][0];
+            let moneyValue = USD[i][1];
 
+            // console.log("log",moneyName, Math.ceil(moneyToReturn / moneyValue))
+            let valid = Math.round(moneyToReturn / moneyValue);
 
-    let result = {status,change}
-    return result
+            if( valid > 1){
+                change.push([moneyName,(moneyValue*valid)/100]);
+                status = "OPEN"
+                moneyToReturn -= moneyValue*valid
+                console.log(moneyName,moneyValue*valid,moneyValue,valid)
+                if (moneyToReturn === 0){
+                    return {status,change}
+                }
+            }
+            i -= 1
+        }
+    }
+
+    return {status,change}
 }
 
-let i = checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+let i = checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
 
 console.log(i)
 
